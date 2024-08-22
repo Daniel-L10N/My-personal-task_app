a67747586ac2ea56ec1467a87b7707e41f822653
Daniel-L10N <alejandrolazolemus@gmail.com>
2024-08-22 11:20:39 -0600

    finish api file and create urls.py from app

--- task_app/api.py
+++ task_app/api.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Task
 from .serializer import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
+    permission_classes = [permissions.AllowAny]
     serializer_class = TaskSerializer
