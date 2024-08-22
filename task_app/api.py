ba08848257bb61af79a08c78a1c24bed774a7149
Daniel-L10N <alejandrolazolemus@gmail.com>
2024-08-22 11:46:01 -0600

    update api

--- task_app/api.py
+++ task_app/api.py
@@ -1,8 +1,19 @@
 from rest_framework import viewsets, permissions
 from .models import Task
 from .serializer import TaskSerializer
+from rest_framework.decorators import action
+from rest_framework.response import responses
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = TaskSerializer
+
+    @action(detail=True, methods=['post'])
+    def done(self, request, pk=None):
+        task = self.get_object()
+        task.done = not task.done
+        task.save()
+        return responses({
+            'status': 'Task done' if task.done else 'task undone'
+            })
