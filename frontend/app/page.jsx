047fba166e31e2cf91943b9645d65970f1591235
Daniel-L10N <alejandrolazolemus@gmail.com>
2024-08-26 10:38:40 -0600

    maked components listtask and formtask

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -3,12 +3,10 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div>
-      <h1>
-        Task App
-      </h1>
-    <h2>form task</h2>
+      <h1>Task App</h1>
+      <h2>form task</h2>
 
-    <h2>list task</h2>
+      <h2>list task</h2>
     </div>
   );
 }
---
+++ frontend/components/ListTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListTask() {
+  return (
+    <div>ListTask
+    
+    </div>
+  )
+}
+
+export default ListTask
