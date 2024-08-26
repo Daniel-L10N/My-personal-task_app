700bb7852e2bfa6b0b17db5fc2ed5216a0b05383
Daniel-L10N <alejandrolazolemus@gmail.com>
2024-08-26 10:41:40 -0600

    mv components from ./project to ./project/app

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-
+import FormTask from "./components/FormTask";
 export default function HomePage() {
   return (
     <div>
       <h1>Task App</h1>
-      <h2>form task</h2>
-
-      <h2>list task</h2>
+      <FormTask></FormTask>
     </div>
   );
 }
