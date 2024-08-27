bcbf3a7425b9cbc82a2c55cca85154f816eb5f08
Daniel-L10N <alejandrolazolemus@gmail.com>
2024-08-27 11:56:21 -0600

    created fetch data from api to listTasks, and ouput on screen

--- frontend/app/components/ListTask.jsx
+++ frontend/app/components/ListTask.jsx
@@ -1,8 +1,28 @@
+// peticiones por parte del servidor
+async function loadTasks(){
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tasks/`)
+  const tasks = await res.json()
+  return tasks
+}
 import React from 'react'
 
-function ListTask() {
+async function ListTask() {
+  const tasks = await loadTasks()
+  console.log(tasks)
+
+  
   return (
-    <div>ListTask
+    <div
+    className='bg-slate-700 p-4 w-full'
+    >
+      <h1>Lista de tareas:</h1>
+      {tasks.map(task => (
+        <div key={task.id} 
+        className='bg-slate-500 px-4 py-3 mb-2 rounded-md'>
+          <h2>{task.title}</h2>
+          <p>{task.description}</p>
+        </div>
+      ))}
     
     </div>
   )
